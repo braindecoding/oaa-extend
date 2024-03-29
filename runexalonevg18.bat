4be018e4ebb602d34b20cdbdd0863f1c589de315
Rolly Maulana Awangga <awangga@gmail.com>
2024-03-29 11:20:57 +0700

    vg

---
+++ runexalonevg18.bat
@@ -0,0 +1,33 @@
+Rem experimentname=str(K)+"_"+str(intermediate_dim)+"_" + str(batch_size)+"_" + str(maxiter)
+
+
+
+python dgmmvangerven.py 18 128 10 1500
+python fidvg.py 18 128 10 1500
+python dgmmvangerven.py 18 128 10 2000
+python fidvg.py 18 128 10 2000
+python dgmmvangerven.py 18 128 10 2500
+python fidvg.py 18 128 10 2500
+python dgmmvangerven.py 18 128 10 3000
+python fidvg.py 18 128 10 3000
+python dgmmvangerven.py 18 128 10 3500
+python fidvg.py 18 128 10 3500
+python dgmmvangerven.py 18 128 10 4000
+python fidvg.py 18 128 10 4000
+python dgmmvangerven.py 18 128 10 4500
+python fidvg.py 18 128 10 4500
+
+python dgmmvangerven.py 18 128 20 1500
+python fidvg.py 18 128 20 1500
+python dgmmvangerven.py 18 128 20 2000
+python fidvg.py 18 128 20 2000
+python dgmmvangerven.py 18 128 20 2500
+python fidvg.py 18 128 20 2500
+python dgmmvangerven.py 18 128 20 3000
+python fidvg.py 18 128 20 3000
+python dgmmvangerven.py 18 128 20 3500
+python fidvg.py 18 128 20 3500
+python dgmmvangerven.py 18 128 20 4000
+python fidvg.py 18 128 20 4000
+python dgmmvangerven.py 18 128 20 4500
+python fidvg.py 18 128 20 4500
