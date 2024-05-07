e9f65a261db15d9e852c449a10dabfa785cbca55
Rolly Maulana  Awangga <rolly@awang.ga>
2024-05-07 13:08:09 +0700

    tambah iterasi sampai 13000

--- runmy.bat
+++ runmy.bat
@@ -5,7 +5,7 @@ setlocal enabledelayedexpansion
 rem Define parameters
 set "intermediate_dims=(128 256 512)"
 set "batch_sizes=(10 20 30 40 50)"
-set "max_iters=(500 1000 1500)"
+set "max_iters=(500 1000 1500 2000 2500 3000 3500 4000 4500 5000 5500 6000 6500 7000 7500 8000 8500 9000 9500 10000 10500 11000 11500 12000 12500 13000)"
 rem set "latent=(3 6 12 18)"
 set "latent=(9 15)"
 
